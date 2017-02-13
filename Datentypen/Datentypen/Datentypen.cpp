aa9eb352cfe0b91022710ea0a9de326e8cedbcaa
dkloubert <dkloubert@PC01100756>
2017-02-13 10:57:46 +0100

    final

--- Datentypen/Datentypen/Datentypen.cpp
+++ Datentypen/Datentypen/Datentypen.cpp
@@ -25,7 +25,7 @@ int main() {
 
 	// Konstante in Main
 
-	const int x = 20;
+	// const int x = 20;
 
 	// Was ist eine Variable
 	// max Zeichen für Bezeichner ist 31
@@ -63,15 +63,54 @@ int main() {
 
 	// Macro
 
-	int_fast32_t i32;
+/*	int_fast32_t i32;
 	printf("int_fast32 Wert eingeben: \n");
 	scanf("%" SCNdFAST32, &i32);
 	printf("i32 : " "%10" PRIdFAST32 "\n\n", i32);
-
+*/
 	printf("+++++++++++++++Gleitkomma+++++++++++++++\n\n");
 
 	printf("Der Float Datentyp hat die Dateigroesse %i in Bit\n"
-		"Max Wert: %E\nKleinster positiver Wert: %E\nGenauigkeit: %d in Stellen\n", sizeof(float) * 8, FLT_MAX, FLT_MIN);
+		"Max Wert: %E\nKleinster positiver Wert: %E\nGenauigkeit: %d in Stellen\n", sizeof(float) * 8, FLT_MAX, FLT_MIN, FLT_DIG);
+
+	printf("Der Double Datentyp hat die Dateigroesse %i in Bit\n"
+		"Max Wert: %E\nKleinster positiver Wert: %E\nGenauigkeit: %d in Stellen\n", sizeof(double) * 8, DBL_MAX, DBL_MIN, DBL_DIG);
+
+	printf("Der Long Double Datentyp hat die Dateigroesse %i in Bit\n"
+		"Max Wert: %E\nKleinster positiver Wert: %E\nGenauigkeit: %d in Stellen\n", sizeof(long double) * 8, LDBL_MAX, LDBL_MIN, LDBL_DIG);
+
+	float x = 0.939717f;
+	float dollarFloat = 100000.22f;
+	double ergF;
+
+	double y = 0.939717;
+	double dollarDouble = 100000.22;
+	double ergD;
+
+	printf("%f Euro in Float\n", ergF = dollarFloat * x);
+	printf("%f Euro in Double\n\n", ergD = dollarDouble * y);
+
+	// Komplexe Gleitpunkte
+
+	_Dcomplex dc1 = { 3.0, 2.0 };
+	_Fcomplex fc1 = { 20.00, 30.00 };
+	_C_double_complex lc1 = { 200.00, 300.00 };
+
+	printf("%f %f\n\n", creal(dc1), cimag(dc1));
+
+	printf("+++++++++++++++Zeichen+++++++++++++++\n\n");
+
+	char zeichen = 'H';
+	char hallo[6] = "Hallo";
+
+	printf("Der Datentyp char hat die Dateigroesse von %i Bit\n"
+		"Min Wert: %i Max Wert: %i\n", sizeof(char) * 8, CHAR_MIN, CHAR_MAX);
+
+	printf("Der Datentyp unsigned char hat die Dateigroesse von %i Bit\n"
+		"Max Wert: %i\n\n", sizeof(unsigned char) * 8, UCHAR_MAX);
+
+	bool b3 = false;
+
 
 	return 0;
 }
