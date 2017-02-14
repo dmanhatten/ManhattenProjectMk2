4f4ce01a2e88d10dfdade1d034d5ba0593343d2f
dkloubert <dkloubert@PC01100756>
2017-02-14 12:35:03 +0100

    Kredit in Funktion

--- kreditLaufz/kreditLaufz/kreditLaufz.cpp
+++ kreditLaufz/kreditLaufz/kreditLaufz.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+int kredit();
+
 int main() {
+
+	kredit();
+
+	return 0;
+}
+
+int kredit() {
 
 	double kreditbetrag = 0, zinssatz = 0;
 	int laufzeit = 0;
@@ -15,8 +24,8 @@ int main() {
 	scanf("%lf", &zinssatz);
 
 	q = 1 + (zinssatz / 100) / 12;
-	belastung = kreditbetrag * pow(q, laufzeit) * (q - 1) / (pow(q, laufzeit) - 1);
-	printf("Monatliche Belastung: %.2lf\n", belastung);
+	belastung = kreditbetrag * (pow(q, laufzeit) * (q - 1) / (pow(q, laufzeit) - 1));
+	printf("Monatliche Belastung von %.2lf ueber %i Monate\n", belastung, laufzeit);
 
 	return 0;
 }
