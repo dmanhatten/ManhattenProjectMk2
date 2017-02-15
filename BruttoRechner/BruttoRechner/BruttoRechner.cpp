b1b44d7e4bfa47202b3905db2bc220fb2e518090
dkloubert <dkloubert@PC01100756>
2017-02-15 08:41:03 +0100

    Übung Teil 4

---
+++ BruttoRechner/BruttoRechner/BruttoRechner.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+	float nettoBetrag = 0, bruttoBetrag = 0;
+	float steuersatz = 0;
+
+	printf("---------------Brutto Rechner---------------\n\n");
+	printf("Geben Sie einen Netto Betrag an: ");
+	scanf("%f", &nettoBetrag);
+
+	printf("\nGeben Sie einen Steuersatz als Ganzzahl ein: ");
+	scanf("%f", &steuersatz);
+
+	bruttoBetrag = nettoBetrag * (1 + steuersatz / 100);
+	printf("\nDer Bruttobetrag lautet: %.2f\n", bruttoBetrag);
+
+	
+	return 0;
+}
